ad41e0dd969c0d42e5ee914d7d4ea82378facdfb
Lukas <lichtenrider@hotmail.de>
2025-06-04 12:53:03 +0200

    Updated for newer version of Mason

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -593,18 +593,7 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
+  -- ensure_installed = vim.tbl_keys(servers),
 }
 
 -- [[ Configure nvim-cmp ]]
